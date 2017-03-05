b1bb3de30bd61aeba2117a8d9274be41c8fbe83c
Enol Iglesias <enoliglesias@gmail.com>
2017-03-05 19:50:47 +0100

    Add cli.ex with moduledoc

---
+++ lib/issues/cli.ex
@@ -0,0 +1,7 @@
+defmodule Issues.CLI do
+  @moduledoc """
+  Handle the command line parsing and the dispatch to
+  the various functions that end up generating a table
+  of the last _n_ issues in a github project
+  """
+end
