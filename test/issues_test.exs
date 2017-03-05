95d6b18ebc054745acf83169065f8c496b06c2b3
Enol Iglesias <enoliglesias@gmail.com>
2017-03-05 22:33:40 +0100

    Add parse args in cli.ex and some test

--- test/issues_test.exs
+++ test/issues_test.exs
@@ -2,7 +2,19 @@ defmodule IssuesTest do
   use ExUnit.Case
   doctest Issues
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import Issues.CLI, only: [parse_args: 1]
+
+  test ":help returned by option parsing with -h and --help" do
+    assert parse_args(["-h", "wadus"]) == :help
+    assert parse_args(["--help", "wadus"]) == :help
   end
+
+  test "three values returned if three given" do
+    assert parse_args(["enol", "issues", "10"]) == {"enol", "issues", 10}
+  end
+
+  test "three values returned with default count when two params given" do
+    assert parse_args(["enol", "issues"]) == {"enol", "issues", 4}
+  end
+
 end
